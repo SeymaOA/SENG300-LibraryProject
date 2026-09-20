276be619c62db9dc0a31c15f89232ea0bd6f8d06
Seyma (Ozcan) Aguilar <82340301+SeymaOA@users.noreply.github.com>
2026-09-20 07:03:55 -0400

    Implement sorting of books by authors in main method
    Added sorting functionality for books by authors and output to console.

--- LibraryProject Week2/src/Main.java
+++ LibraryProject Week2/src/Main.java
@@ -3,15 +3,19 @@ package Week2LibraryProject;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
-
+import static java.lang.System.out;
 
 public class Main{
 	public static void main(String[] args) throws FileNotFoundException{
 		Library library = new Library();
 		library.readCSV("C:/Users/seymapc/eclipse-workspace/Thinkdast-SENG300/src/books.csv");
+
+		library.sortByAuthors(library.books); // should have a button to click for sort in gui
 		
+		out.println("Sorted by authors: ");
 		
-		
-		
+		for(int i = 0; i < library.books.size(); i++) {
+			out.println(library.books.get(i));
+		}
 	}
 }
