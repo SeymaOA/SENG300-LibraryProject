a51b91b2bb2295f31a01c092d5ade8b0238e7965
Seyma (Ozcan) Aguilar <82340301+SeymaOA@users.noreply.github.com>
2026-09-22 20:53:56 -0400

    Update Main.java

--- LibraryProject Week2/src/Main.java
+++ LibraryProject Week2/src/Main.java
@@ -4,8 +4,20 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+/* 
+ * Runs basics console tests for the ArrayList library.
+ */
 public class Main {
 
+	/*
+	 * Starts the console tests.
+	 * Loads books from CSV file and prints the total number loaded.
+	 * Sorts the book by author in ascending order and displays.
+	 * Performs binary search for book ID and prints the results.
+	 * 
+	 * @param args comman-line arguments; the first argument is an
+	 * optional CSV file path. 
+	 */
 	public static void main(String[] args) {
 
 		String fileName =args.length > 0 ? args[0] : "data/books_1.csv";
