89b0a5d39450444f8d6611b83200a89c00301208
Seyma (Ozcan) Aguilar <82340301+SeymaOA@users.noreply.github.com>
2026-09-22 20:32:53 -0400

    Update Main.java

--- LibraryProject Week2/src/Main.java
+++ LibraryProject Week2/src/Main.java
@@ -6,83 +6,47 @@ import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        String fileName =
-                args.length > 0 ? args[0] : "data/books.csv";
+		String fileName =args.length > 0 ? args[0] : "data/books_1.csv";
 
-        Library library = new Library();
+		Library library = new Library();
 
-        try {
+		try {
 
-            library.readCSV(fileName);
+			library.readCSV(fileName);
 
-            System.out.println(
-                    "Books loaded: "
-                    + library.getBooksList().size());
-
-
-            // Sort authors ascending
-            library.sortByAuthors(true);
-
-
-            System.out.println(
-                    "\nFirst 10 books sorted by authors:");
-
-            List<Book> books =
-                    library.getBooksList();
+			System.out.println("Books loaded: "+ library.getBooksList().size());
+			// Sort authors ascending
+			library.sortByAuthors(true);
+			System.out.println("\nFirst 10 books sorted by authors:");
+			List<Book> books =	library.getBooksList();
+			for (int i = 0;	i < Math.min(10, books.size());	i++) {
+				System.out.println(	books.get(i));
+			}
 
 
-            for (int i = 0;
-                 i < Math.min(10, books.size());
-                 i++) {
+			System.out.println("\nBinary search for book_id 1:");
+			List<Book> searchResult =library.binarySearch("1","book_id");
 
-                System.out.println(
-                        books.get(i));
-            }
+			if (searchResult.isEmpty()) {
+				System.out.println(	"Book not found.");
+			} else {
 
+				for (Book book : searchResult) {
+					System.out.println(book);
+				}
+			}
 
-            System.out.println(
-                    "\nBinary search for book_id 1:");
+		}
 
+		catch (IOException e) {
+			System.err.println("Could not load: "+ new File(fileName).getAbsolutePath());
+			System.err.println(	e.getMessage());
+		}
 
-            List<Book> searchResult =
-                    library.binarySearch(
-                            "1",
-                            "book_id");
-
-
-            if (searchResult.isEmpty()) {
-
-                System.out.println(
-                        "Book not found.");
-
-            } else {
-
-                for (Book book : searchResult) {
-
-                    System.out.println(book);
-                }
-            }
-
-        }
-
-        catch (IOException e) {
-
-            System.err.println(
-                    "Could not load: "
-                    + new File(fileName)
-                    .getAbsolutePath());
-
-            System.err.println(
-                    e.getMessage());
-        }
-
-        catch (IllegalArgumentException e) {
-
-            System.err.println(
-                    "Error: "
-                    + e.getMessage());
-        }
-    }
+		catch (IllegalArgumentException e) {
+			System.err.println("Error: "+ e.getMessage());
+		}
+	}
 }
